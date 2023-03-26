79264b4df523444e19cd25d2c74c56585f9dfd20
berkakaz <berk.akaz@gmail.com>
2023-03-26 15:23:31 +0300

    tamamlandı

--- frontend/App.js
+++ frontend/App.js
@@ -1,31 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import useLocalStorage from "../hooks/localStorageKullan";
-
-
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import geceModuAc from "./hooks/geceModuAc";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [geceModuAc, setGeceModuAc] = useLocalStorage("nachtMode" ,false);
-  const [userInf, setuserInf] = useLocalStorage("loggedInUser", {
-    userName: "yokke",
-    userEmail: "vidivici@vokke.com",
+  const [geceModu, setGeceModu] = geceModuAc(false);
 
-  });
   useEffect(() => {
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+      .then((res) => setCoinData(res.data))
+      .catch((err) => console.log(err));
   }, []);
   return (
-    <div className={geceModuAc ? "dark-mode App" : "App"}>
-      <Navbar geceModuAc={geceModuAc} setGeceModuAc={setGeceModuAc} />
+    <div className={geceModu ? "dark-mode App" : "App"}>
+      <Navbar geceModu={geceModu} setGeceModu={setGeceModu} />
       <Charts coinData={coinData} />
     </div>
   );
